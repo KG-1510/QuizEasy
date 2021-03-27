3f063d8ecb0576bfb717ad6efa2be615bf3835ec
Kushagra Gupta <guptakushagra15.10@gmail.com>
2021-03-27 21:59:40 +0530

    ✨ Add loading screen

--- static/scripts/upload.js
+++ static/scripts/upload.js
@@ -4,7 +4,13 @@ $("#file-browser").click(function (e) {
   e.preventDefault();
   $("#file-upload").trigger("click");
 });
-
+document.querySelector(".submitDoc").addEventListener("click", () => {
+    document.querySelector("#upload-message").style.display = "block";
+    document.body.style.cursor = "wait";
+    document.querySelector(".wrapper").style.cursor = "wait";
+    document.querySelector(".submitDoc").innerHTML = "Please wait..."
+    document.querySelector(".upload-container").style.opacity = "0.3"; 
+});
 const fileInput = document.querySelector(
   "input[type=file]"
 );
@@ -15,15 +21,15 @@ fileInput.onchange = () => {
     document.querySelector('.choose-file-label').style.display = "none";
     fileName.textContent = uploadedFileName;
 
-    if(uploadedFileName.includes(".pdf")){
+    if (uploadedFileName.includes(".pdf")) {
       document.querySelector(".pdf").classList.add('pdficon')
       document.querySelector(".txt").classList.remove('txticon')
     }
-    
-    else if(uploadedFileName.includes(".txt")){
+
+    else if (uploadedFileName.includes(".txt")) {
       document.querySelector(".txt").classList.add('txticon')
       document.querySelector(".pdf").classList.remove('pdficon')
-    
+
     }
   }
 };
